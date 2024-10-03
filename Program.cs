90d4df26af2bc4534385673651cd1fb436b8275a
ElisaRumSolberg <rumeysaince07@gmail.com>
2024-10-03 10:46:52 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace lesson1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("hei verden!");
+
+var elevName="Elisa";
+var elevEttername="Solberg";
+var elevFullname= "Elisa Solberg";
+var elevNummeret="123";
+var ElevFødselDato="23.08.1991";
+var ElevAdressa="Øvre Holen 5";
+
+
+var produkt1=500;
+var produkt2=300;
+var produkt3=658;
+var sum=produkt1+produkt2+produkt3;
+Console.WriteLine(sum);
+
+    }
+}
+
+    
+
