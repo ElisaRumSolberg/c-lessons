6752f821b6ac859302daa4b261aa96c5a138bafc
ElisaRumSolberg <rumeysaince07@gmail.com>
2024-10-04 11:05:02 +0200

    2. commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace lesson1;
 
@@ -6,6 +7,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Console.WriteLine("Hello, World!");
 
         Console.WriteLine("hei verden!");
@@ -23,6 +25,44 @@ var produkt2=300;
 var produkt3=658;
 var sum=produkt1+produkt2+produkt3;
 Console.WriteLine(sum);
+
+// value types
+byte tall =10;
+//artabilecek sayilarda int kullanmak daha akillica
+double rente=1.6;
+double rente = 2;
+// float bir deger tanimlandiginda sonuna f eklenmeli
+float rente2= 1.8f;
+decimal rente3= 1.5m;
+// decimal icinde sonuna m ekliyoruz
+char kjøn= 'K';
+string kjøn2="Kvinne";
+string produktName="Samsung S24";
+Boolean erDetSalg= true;
+*/
+//================================================
+// String
+Console.Write("name: ");
+String? name= Console.ReadLine();
+
+Console.Write("ettername: ");
+String? ettername= Console.ReadLine();
+
+Console.Write("alder: ");
+var alder=Console.ReadLine();
+
+// 1. method
+//String melding = name +" "+ ettername+ " er "+alder+ " år gammel" ;
+
+//2. method
+// dolar isaterinden sonra cift tirnak acip degiskenleri {} icine yazip digerlerini normal yaziyoruz
+string melding= $"{name} {ettername} er{alder} år gammel";
+
+
+Console.WriteLine(melding);
+
+
+
 
     }
 }
--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d4df26af2bc4534385673651cd1fb436b8275a")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
